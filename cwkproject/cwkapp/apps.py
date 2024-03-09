53af64c8915b3c1f326698b053d7b2473aab0933
Rejus Bulevicius <ed20rb5@leeds.ac.uk>
2024-03-09 19:06:08 +0000

    ORM Made, not migrated

---
+++ cwkproject/cwkapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CwkappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cwkapp'
---
+++ cwkproject/cwkapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+catergory_choices = [('pol', "Political"), ('art', "Art"), ('tech', "Technical"), ('trivia', "Trivial")]
+region_choices = [('uk', "British News"), ('eu', "European News"), ('w', "World News")]
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=128)
+    username = models.CharField(max_length=64, unique=True)
+    password = models.CharField(max_length=128)
+
+class news(models.Model):
+    uniquekey = models.AutoField(primary_key=True)
+    headline = models.CharField(max_length=64)
+    catergory = models.CharField(max_length=6, choices=catergory_choices)
+    region = models.CharField(max_length=2, choices=region_choices)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    date = models.DateField()
+    details = models.CharField(max_length=128)
